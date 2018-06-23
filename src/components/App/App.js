4101e6573167750bbb011fbbd2103a489976ede2
Dan Ross <dr.ross15@gmail.com>
2018-06-23 12:37:35 -0500

    add axios get all for all gallery items

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,28 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      galleryList: []
+    }
+  }
+
+  componentDidMount() {
+    this.getGallery();
+  }
+
+  getGallery() {
+    axios.get('/gallery')
+      .then(response => {
+        console.log('response:', response.data);
+        this.setState({galleryList: 
+          [...this.state.galleryList, ...response.data]})
+          console.log('galleryList:', this.state.galleryList);
+      }).catch(err => {
+        console.log('err:', err);
+      })
+  }
   render() {
     return (
       <div className="App">
