c90dcb77036a3494bc93265707b67739243fdd9f
Dan Ross <dr.ross15@gmail.com>
2018-06-23 15:30:52 -0500

    flex display items

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './GalleryItem.css'
 
 class GalleryItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: true
+        }
+    }
     likePhoto = () => {
         console.log('id:', this.props.item.id);
         
@@ -14,19 +21,35 @@ class GalleryItem extends Component {
 
     handleClick = () => {
         console.log('in handleclick to show:', this.props.item.description);
-        
+        console.log('image before click:', this.state.image);
+        this.state.image = !this.state.image;
+        console.log('image after click:', this.state.image);
+        this.forceUpdate();
     }
 
     render() {
-        return (
-            <div>
-                <img onClick={this.handleClick} src={this.props.item.path} height='300' width='300' alt={this.props.item.description}/>
-                <br/>
-                <button onClick={this.likePhoto}>I like this.</button>
-                <br/><br/>
-                <p>{this.props.item.likes} like this</p>
-            </div>
-        )
+        if(this.state.image) {
+            return (
+                <div>
+                    <img onClick={this.handleClick} src={this.props.item.path} height='400' width='400' alt={this.props.item.description}/>
+                    <br/>
+                    <button onClick={this.likePhoto}>I like this.</button>
+                    <br/><br/>
+                    <p>{this.props.item.likes} like this</p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div className="description" onClick={this.handleClick} >{this.props.item.description}</div>
+                    <br/>
+                    <button onClick={this.likePhoto}>I like this.</button>
+                    <br/><br/>
+                    <p>{this.props.item.likes} like this</p>
+                </div>  
+            )
+        }
+        
     }
 }
 
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem';
+import './GalleryList.css';
 
 class GalleryList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
     render() {
         return (
-            <div>
+            <div className="item">
                 {this.props.galleryList.map((item, i) => <GalleryItem item={item} key={item.id} getGallery={this.props.getGallery} galleryList={this.props.galleryList}/>)}
             </div>
         )
