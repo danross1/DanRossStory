9e4a3fe1790a41cf224cb4d0d3483b8d8c7e8848
Dan Ross <dr.ross15@gmail.com>
2018-06-23 21:39:56 -0500

    gave it a once over

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: ['Aldrich', 'sans-serif']
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
